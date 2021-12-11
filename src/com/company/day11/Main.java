aba2b6f576477bb7c9048b1c332a9d32a717736a
togamid <gaisserto80670@th-nuernberg.de>
2021-12-11 10:28:30 +0100

    Tag 11

---
+++ src/com/company/day11/Main.java
@@ -0,0 +1,103 @@
+package com.company.day11;
+
+import com.company.Util;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+public class Main {
+    private static long flashCount = 0;
+
+    public static void main(String[] args) {
+        List<String> inputString = Util.loadFile("src/com/company/day11/input.txt");
+        Integer[][] map = new Integer[inputString.size()][inputString.get(0).length()];
+
+        for (int i = 0; i < map.length; i++) {
+            map[i] = Util.parseArrayInt(inputString.get(i).split(""));
+        }
+        Integer[][] lastFlashRound = new Integer[inputString.size()][inputString.get(0).length()];
+
+        for(int round = 0; round < 300; round++) {
+
+            for (int y = 0; y < map.length; y++) {
+                for (int x = 0; x < map[0].length; x++) {
+                    map[y][x]++;
+                }
+            }
+
+
+            for (int y = 0; y < map.length; y++) {
+                for (int x = 0; x < map[0].length; x++) {
+                    testFlash(x, y, round, map, lastFlashRound);
+                }
+            }
+            int octopussesFlashedThisRound = 0;
+            for (int y = 0; y < map.length; y++) {
+                for (int x = 0; x < map[0].length; x++) {
+                    if(lastFlashRound[y][x] != null && lastFlashRound[y][x] == round) {
+                        octopussesFlashedThisRound++;
+                        map[y][x] = 0;
+                    }
+                }
+            }
+            if(octopussesFlashedThisRound == 100) {
+                System.out.println("Result 2: " + (round + 1));
+                break;
+            }
+
+        }
+    }
+    private static void output(Integer[][] array){
+        for(int y = 0; y < array.length; y ++) {
+            for(int x = 0; x < array[0].length; x++) {
+                System.out.print(array[y][x]);
+            }
+            System.out.println();
+        }
+    }
+
+    private static void testFlash(int x, int y, int round, Integer[][] map, Integer[][] lastFlashRound) {
+        if(map[y][x] <= 9 || (lastFlashRound[y][x] != null && lastFlashRound[y][x] == round)) {
+            return;
+        }
+        flashCount++;
+        lastFlashRound[y][x] = round;
+        if(y-1 >= 0) {
+            map[y-1][x]++;
+            testFlash(x, y-1, round, map, lastFlashRound);
+        }
+        if(y+1 <map.length ){
+            map[y+1][x]++;
+           testFlash(x, y+1, round, map, lastFlashRound);
+        }
+        if(x+1 <map[0].length ){
+            map[y][x+1]++;
+            testFlash(x+1, y, round, map, lastFlashRound);
+        }
+        if(x-1 >=0 ){
+            map[y][x-1]++;
+            testFlash(x-1, y, round, map, lastFlashRound);
+        }
+        if(y-1 >= 0 && x-1 >=0) {
+            map[y-1][x-1]++;
+            testFlash(x-1, y-1, round, map, lastFlashRound);
+        }
+        if(y-1 >= 0 && x+1 <map[0].length) {
+            map[y-1][x+1]++;
+            testFlash(x+1, y-1, round, map, lastFlashRound);
+        }
+        if(y+1 <map.length && x+1 <map[0].length) {
+            map[y+1][x+1]++;
+            testFlash(x+1, y+1, round, map, lastFlashRound);
+        }
+        if(y+1 <map.length && x-1 >=0) {
+            map[y+1][x-1]++;
+            testFlash(x-1, y+1, round, map, lastFlashRound);
+        }
+
+    }
+
+}
